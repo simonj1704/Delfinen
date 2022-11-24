e0c682e6156ed65bc8f5d09bcd315c65247597d6
Anders <113690119+acb100@users.noreply.github.com>
2022-11-24 13:02:36 +0100

    Added option to add coach to elite swimmer.

--- src/memberTypes/EliteSwimmer.java
+++ src/memberTypes/EliteSwimmer.java
@@ -3,12 +3,22 @@ package src.memberTypes;
 import src.Member;
 
 public class EliteSwimmer extends Member {
+    private String coach;
 
-    public EliteSwimmer(String name, int age, boolean isPassive, boolean hasArrears) {
+    public EliteSwimmer(String name, int age, String coach, boolean isPassive, boolean hasArrears) {
         setName(name);
         setAge(age);
         setTypeOfSwimmer("Elite Swimmer");
+        setCoach(coach);
         setIsPassive(isPassive);
         setHasArrears(hasArrears);
+    }
+
+    public void setCoach(String coach){
+        this.coach = coach;
+    }
+
+    public String getCoach() {
+        return coach;
     }
 }
