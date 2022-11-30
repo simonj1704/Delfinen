ececcefdcfd43d5cb8bd28bb93ba0741502b68fa
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2022-11-30 09:23:06 +0100

    Moved TimesHandler

--- src/MemberHandler.java
+++ src/MemberHandler.java
@@ -3,6 +3,7 @@ package src;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,6 +14,15 @@ public class MemberHandler {
     Scanner in = new Scanner(System.in);
     private boolean isRunning = true;
     private String input;
+    private String inputDiscipline;
+    private int trainingTimeSeconds;
+    private int trainingTimeMilSeconds;
+    private String tourneyName;
+    private String placement;
+    private int placementTimeSeconds;
+    private int placementTimeMiliseconds;
+    LocalDate date = LocalDate.now();
+    ArrayList<TrainingTime> trainingTimes = new ArrayList<>();
 
     public MemberHandler() {
     }
@@ -151,5 +161,47 @@ public class MemberHandler {
             }
         }
         return hasArrears;
+    }
+
+    //Testing
+    EliteSwimmer eliteSwimmer = new EliteSwimmer("Michael", 42, "Maria", "Butterfly");
+    //
+
+
+
+    public void createTrainingTime() {
+        System.out.print("Input Discipline ");
+        inputDiscipline = in.nextLine();
+        System.out.print("Input Training Time in Seconds: ");
+        trainingTimeSeconds = in.nextInt();
+        in.nextLine();
+        System.out.print("Input Training Time in Milliseconds: ");
+        trainingTimeMilSeconds = in.nextInt();
+        in.nextLine();
+
+        TrainingTime newTime = new TrainingTime(date, trainingTimeSeconds, trainingTimeMilSeconds, inputDiscipline);
+        addTime(newTime);
+    }
+
+    /*public void createTournamentResult(){
+        System.out.println("Input tourney name");
+        tourneyName = in.nextLine();
+        System.out.println("Input tournament placement");
+        placement = in.nextLine();
+        System.out.println("Input swimtime seconds");
+        placementTimeSeconds = in.nextInt();
+        in.nextLine();
+        System.out.println("Input swimtime ms");
+        placementTimeMiliseconds = in.nextInt();
+        in.nextLine();
+
+        TournamentBoard tournamentBoard = new TournamentBoard(date,tourneyName,placement,placementTimeSeconds,
+                placementTimeMiliseconds);
+
+    }*/
+
+
+    public void addTime(TrainingTime newTime) {
+        trainingTimes.add(newTime);
     }
 }
