0ff2185bb8ce1a395f609b41c527da5cb1b8552b
simonj1704 <simo915m@stud.kea.dk>
2022-12-02 12:56:59 +0100

    Added PRinttimes method

--- src/EliteSwimmer.java
+++ src/EliteSwimmer.java
@@ -87,9 +87,10 @@ public class EliteSwimmer extends Member {
     
     public void printTimes(){
         for (int i = 0; i < trainingTimes.size(); i++) {
-            
+            System.out.printf("%s %d %d %s",trainingTimes.get(i).getDate(),
+                    trainingTimes.get(i).getTrainingSeconds(), trainingTimes.get(i).getTrainingMilSeconds(),
+                    trainingTimes.get(i).getDiscipline());
         }
-        System.out.printf("");
     }
 
     @Override
